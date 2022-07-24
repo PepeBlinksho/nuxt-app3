97225b5d3b253192149a72b60e6260c3dce5618e
sikeda <sikeda@fourmix.co.jp>
2022-07-24 20:47:15 +0900

    init

---
+++ app.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+  const route = useRoute()
+</script>
+
+<template>
+  <nav>
+    <NuxtLink>Home</NuxtLink>
+    <NuxtLink>about</NuxtLink>
+    <NuxtLink>parent</NuxtLink>
+  </nav>
+</template>
